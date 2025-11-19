f33ac3c3ec98dd4a4bd34181158d2e510e97cfa6
JMaximus76 <jmaximus76@proton.me>
2025-11-18 21:57:23 -0800

    Fixed error files to be in line with jcl

--- include/jcl/error.h
+++ include/jcl/error.h
@@ -1,5 +1,5 @@
-#ifndef JFS_ERROR_H
-#define JFS_ERROR_H
+#ifndef JCL_ERROR_H
+#define JCL_ERROR_H
 
 #include <dirent.h>
 #include <errno.h>
@@ -7,36 +7,19 @@
 #include <string.h>
 #include <sys/stat.h>
 
-// TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP
-#include <stdio.h>
-#define LOG_STR "\033[1;31m%s\033[0m: \033[35m%s\033[0m  errno: %s\n"
-#define RES_STR "\033[1;36m%s\033[0m: \033[35m%s\033[0m\n"
-
-#define LOG_ERR printf(LOG_STR, jfs_err_str(err), __func__, strerror(errno))
-#define RES_ERR                                      \
-    do {                                             \
-        printf(RES_STR, jfs_err_str(err), __func__); \
-        *err = JFS_OK;                               \
-    } while (0)
-
-#define IGNORE_ERR *err = JFS_OK;
-// TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP
 
 #define VOID_RETURN_ERR \
     do {                \
-        LOG_ERR;        \
         return;         \
     } while (0)
 
 #define NULL_RETURN_ERR \
     do {                \
-        LOG_ERR;        \
         return NULL;    \
     } while (0)
 
 #define VAL_RETURN_ERR(val_var) \
     do {                        \
-        LOG_ERR;                \
         return (val_var);       \
     } while (0)
 
@@ -102,76 +85,55 @@
     do {                            \
         if (*err == (from_var)) {   \
             *err = (to_var);        \
-            printf("remap err\n");  \
         }                           \
     } while (0)
 
 #define WUR __attribute__((warn_unused_result))
 
-#define JFS_ERROR_LIST             \
-    X(JFS_OK)                      \
-    X(JFS_ERR_SYS)                 \
-    X(JFS_ERR_INTER)               \
-    X(JFS_ERR_AGAIN)               \
-    X(JFS_ERR_ACCESS)              \
-    X(JFS_ERR_INVAL_PATH)          \
-    X(JFS_ERR_ARG)                 \
-    X(JFS_ERR_EMPTY)               \
-    X(JFS_ERR_FULL)                \
-    X(JFS_ERR_BAD_CONF)            \
-    X(JFS_ERR_GETADDRINFO)         \
-    X(JFS_ERR_LAN_HOST_UNREACH)    \
-    X(JFS_ERR_CONNECTION_ABORT)    \
-    X(JFS_ERR_CONNECTION_RESET)    \
-    X(JFS_ERR_PIPE)                \
-    X(JFS_ERR_MUTEX_BUSY)          \
-    X(JFS_ERR_COND_BUSY)           \
-    X(JFS_ERR_COND_TIMED_OUT)      \
-    X(JFS_ERR_FIO_PATH_LEN)        \
-    X(JFS_ERR_FIO_NAME_LEN)        \
-    X(JFS_ERR_FIO_PATH_OVERFLOW)   \
-    X(JFS_ERR_FIO_FILE_END)        \
-    X(JFS_ERR_FW_STATE)            \
-    X(JFS_ERR_FW_SKIP)             \
-    X(JFS_ERR_FW_FAIL)             \
-    X(JFS_ERR_FW_UNSUPPORTED)      \
-    X(JFS_ERR_FW_UNKNOWN)          \
-    X(JFS_ERR_NS_BAD_ACCEPT)       \
-    X(JFS_ERR_NS_BAD_ADDR)         \
-    X(JFS_ERR_NS_CONNECTION_CLOSE) \
-    X(JFS_ERR_BST_BAD_KEY)
-
 typedef enum {
-#define X(name) name,
-    JFS_ERROR_LIST
-#undef X
-} jfs_err_t;
+    JCL_ERR_OK = 1,
+    JCL_ERR_SYS,
+    JCL_ERR_INTER,
+    JCL_ERR_AGAIN,
+    JCL_ERR_ACCESS,
+    JCL_ERR_INVAL_PATH,
+    JCL_ERR_ARG,
+    JCL_ERR_EMPTY,
+    JCL_ERR_FULL,
+    JCL_ERR_BAD_CONF,
+    JCL_ERR_GETADDRINFO,
+    JCL_ERR_LAN_HOST_UNREACH,
+    JCL_ERR_CONNECTION_ABORT,
+    JCL_ERR_CONNECTION_RESET,
+    JCL_ERR_PIPE,
+    JCL_ERR_MUTEX_BUSY,
+    JCL_ERR_COND_BUSY,
+    JCL_ERR_COND_TIMED_OUT,
+} jcl_err_t;
 
-const char *jfs_err_str(const jfs_err_t *err);
-
-void            *jfs_malloc(size_t size, jfs_err_t *err) WUR;
-void            *jfs_realloc(void *ptr, size_t size, jfs_err_t *err) WUR;
-void             jfs_lstat(const char *path, struct stat *stat_init, jfs_err_t *err);
-DIR             *jfs_opendir(const char *path, jfs_err_t *err) WUR;
-void             jfs_shutdown(int sock_fd, int how, jfs_err_t *err);
-struct addrinfo *jfs_getaddrinfo(const char *name, const char *port_str, const struct addrinfo *hints, jfs_err_t *err) WUR;
-void             jfs_bind(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jfs_err_t *err);
-void             jfs_listen(int sock_fd, int backlog, jfs_err_t *err);
-int              jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jfs_err_t *err) WUR;
-void             jfs_connect(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jfs_err_t *err);
-size_t           jfs_recv(int sock_fd, void *buf, size_t size, int flags, jfs_err_t *err) WUR;
-size_t           jfs_send(int sock_fd, const void *buf, size_t size, int flags, jfs_err_t *err) WUR;
-int              jfs_socket(int domain, int type, int protocol, jfs_err_t *err) WUR;
-void             jfs_close(int close_fd, jfs_err_t *err);
-void             jfs_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr, jfs_err_t *err);
-void             jfs_mutex_destroy(pthread_mutex_t *mutex, jfs_err_t *err);
-void             jfs_mutex_trylock(pthread_mutex_t *mutex, jfs_err_t *err);
-void             jfs_cond_destroy(pthread_cond_t *cond, jfs_err_t *err);
-void             jfs_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *time, jfs_err_t *err);
-int              jfs_eventfd(unsigned int initval, int flags, jfs_err_t *err) WUR;
-size_t           jfs_read(int fd, void *buf, size_t size, jfs_err_t *err) WUR;
-size_t           jfs_write(int fd, const void *buf, size_t size, jfs_err_t *err) WUR;
-void            *jfs_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off, jfs_err_t *err) WUR;
-void            *jfs_aligned_alloc(size_t align, size_t size, jfs_err_t *err) WUR;
+void            *jfs_malloc(size_t size, jcl_err_t *err) WUR;
+void            *jfs_realloc(void *ptr, size_t size, jcl_err_t *err) WUR;
+void             jfs_lstat(const char *path, struct stat *stat_init, jcl_err_t *err);
+DIR             *jfs_opendir(const char *path, jcl_err_t *err) WUR;
+void             jfs_shutdown(int sock_fd, int how, jcl_err_t *err);
+struct addrinfo *jfs_getaddrinfo(const char *name, const char *port_str, const struct addrinfo *hints, jcl_err_t *err) WUR;
+void             jfs_bind(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jcl_err_t *err);
+void             jfs_listen(int sock_fd, int backlog, jcl_err_t *err);
+int              jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jcl_err_t *err) WUR;
+void             jfs_connect(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jcl_err_t *err);
+size_t           jfs_recv(int sock_fd, void *buf, size_t size, int flags, jcl_err_t *err) WUR;
+size_t           jfs_send(int sock_fd, const void *buf, size_t size, int flags, jcl_err_t *err) WUR;
+int              jfs_socket(int domain, int type, int protocol, jcl_err_t *err) WUR;
+void             jfs_close(int close_fd, jcl_err_t *err);
+void             jfs_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr, jcl_err_t *err);
+void             jfs_mutex_destroy(pthread_mutex_t *mutex, jcl_err_t *err);
+void             jfs_mutex_trylock(pthread_mutex_t *mutex, jcl_err_t *err);
+void             jfs_cond_destroy(pthread_cond_t *cond, jcl_err_t *err);
+void             jfs_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *time, jcl_err_t *err);
+int              jfs_eventfd(unsigned int initval, int flags, jcl_err_t *err) WUR;
+size_t           jfs_read(int fd, void *buf, size_t size, jcl_err_t *err) WUR;
+size_t           jfs_write(int fd, const void *buf, size_t size, jcl_err_t *err) WUR;
+void            *jfs_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off, jcl_err_t *err) WUR;
+void            *jfs_aligned_alloc(size_t align, size_t size, jcl_err_t *err) WUR;
 
 #endif
--- src/error.c
+++ src/error.c
@@ -1,4 +1,4 @@
-#include "error.h"
+#include <jcl/error.h>
 #include <asm-generic/errno-base.h>
 #include <asm-generic/errno.h>
 #include <errno.h>
@@ -11,50 +11,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-const char *jfs_err_str(const jfs_err_t *err) {
-    switch (*err) {
-#define X(name)              \
-    case name: return #name;
-        JFS_ERROR_LIST;
-#undef X
-        default: return "failed to map jfs_error_t to string";
-    }
-}
-
-void *jfs_malloc(size_t size, jfs_err_t *err) {
+void *jfs_malloc(size_t size, jcl_err_t *err) {
     void *allocated_memory = malloc(size);
-    NULL_FAIL_IF(allocated_memory == NULL, JFS_ERR_SYS);
+    NULL_FAIL_IF(allocated_memory == NULL, JCL_ERR_SYS);
 
     return allocated_memory;
 }
 
-void *jfs_realloc(void *ptr, size_t size, jfs_err_t *err) {
+void *jfs_realloc(void *ptr, size_t size, jcl_err_t *err) {
     void *allocated_memory = realloc(ptr, size);
-    NULL_FAIL_IF(allocated_memory == NULL, JFS_ERR_SYS);
+    NULL_FAIL_IF(allocated_memory == NULL, JCL_ERR_SYS);
 
     return allocated_memory;
 }
 
-void jfs_lstat(const char *path_str, struct stat *stat_init, jfs_err_t *err) {
+void jfs_lstat(const char *path_str, struct stat *stat_init, jcl_err_t *err) {
     if (lstat(path_str, stat_init) != 0) {
         switch (errno) {
-            case EACCES:  *err = JFS_ERR_ACCESS; break;
+            case EACCES:  *err = JCL_ERR_ACCESS; break;
             case ENOENT:
-            case ENOTDIR: *err = JFS_ERR_INVAL_PATH; break;
-            default:      *err = JFS_ERR_SYS; break;
+            case ENOTDIR: *err = JCL_ERR_INVAL_PATH; break;
+            default:      *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-DIR *jfs_opendir(const char *path_str, jfs_err_t *err) {
+DIR *jfs_opendir(const char *path_str, jcl_err_t *err) {
     DIR *dir = opendir(path_str);
     if (dir == NULL) {
         switch (errno) {
             case ENOENT:
-            case ENOTDIR: *err = JFS_ERR_INVAL_PATH; break;
-            case EACCES:  *err = JFS_ERR_ACCESS; break;
-            default:      *err = JFS_ERR_SYS; break;
+            case ENOTDIR: *err = JCL_ERR_INVAL_PATH; break;
+            case EACCES:  *err = JCL_ERR_ACCESS; break;
+            default:      *err = JCL_ERR_SYS; break;
         }
         NULL_RETURN_ERR;
     }
@@ -62,25 +52,25 @@ DIR *jfs_opendir(const char *path_str, jfs_err_t *err) {
     return dir;
 }
 
-void jfs_shutdown(int sock_fd, int how, jfs_err_t *err) {
+void jfs_shutdown(int sock_fd, int how, jcl_err_t *err) {
     if (shutdown(sock_fd, how) != 0) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-struct addrinfo *jfs_getaddrinfo(const char *name, const char *port_str, const struct addrinfo *hints, jfs_err_t *err) {
+struct addrinfo *jfs_getaddrinfo(const char *name, const char *port_str, const struct addrinfo *hints, jcl_err_t *err) {
     struct addrinfo *result;
     int              status = getaddrinfo(name, port_str, hints, &result);
     if (status != 0) {
         switch (status) {
-            case EAI_AGAIN:  *err = JFS_ERR_AGAIN; break;
+            case EAI_AGAIN:  *err = JCL_ERR_AGAIN; break;
             case EAI_FAIL:
-            case EAI_NONAME: *err = JFS_ERR_LAN_HOST_UNREACH; break;
-            case EAI_SYSTEM: *err = JFS_ERR_SYS; break;
-            default:         *err = JFS_ERR_GETADDRINFO; break;
+            case EAI_NONAME: *err = JCL_ERR_LAN_HOST_UNREACH; break;
+            case EAI_SYSTEM: *err = JCL_ERR_SYS; break;
+            default:         *err = JCL_ERR_GETADDRINFO; break;
         }
         NULL_RETURN_ERR;
     }
@@ -88,29 +78,29 @@ struct addrinfo *jfs_getaddrinfo(const char *name, const char *port_str, const s
     return result;
 }
 
-void jfs_bind(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jfs_err_t *err) {
+void jfs_bind(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jcl_err_t *err) {
     if (bind(sock_fd, addr, addrlen) != 0) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_listen(int sock_fd, int backlog, jfs_err_t *err) {
+void jfs_listen(int sock_fd, int backlog, jcl_err_t *err) {
     if (listen(sock_fd, backlog) != 0) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-int jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jfs_err_t *err) {
+int jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jcl_err_t *err) {
     int accepted_fd = accept(sock_fd, addr, addrlen);
     if (accepted_fd == -1) {
         switch (errno) {
-            case ECONNABORTED: *err = JFS_ERR_CONNECTION_ABORT; break;
+            case ECONNABORTED: *err = JCL_ERR_CONNECTION_ABORT; break;
             case EPROTO:
             case ENOPROTOOPT:
             case EHOSTDOWN:
@@ -118,9 +108,9 @@ int jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jfs_err_t
             case EHOSTUNREACH:
             case EOPNOTSUPP:
             case ENETUNREACH:
-            case EAGAIN:       *err = JFS_ERR_AGAIN; break;
-            case EINTR:        *err = JFS_ERR_INTER; break;
-            default:           *err = JFS_ERR_SYS; break;
+            case EAGAIN:       *err = JCL_ERR_AGAIN; break;
+            case EINTR:        *err = JCL_ERR_INTER; break;
+            default:           *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(-1);
     }
@@ -128,27 +118,27 @@ int jfs_accept(int sock_fd, struct sockaddr *addr, socklen_t *addrlen, jfs_err_t
     return accepted_fd;
 }
 
-void jfs_connect(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jfs_err_t *err) {
+void jfs_connect(int sock_fd, const struct sockaddr *addr, socklen_t addrlen, jcl_err_t *err) {
     if (connect(sock_fd, addr, addrlen) != 0) {
         switch (errno) {
-            case EAGAIN:       *err = JFS_ERR_AGAIN; break;
+            case EAGAIN:       *err = JCL_ERR_AGAIN; break;
             case ECONNREFUSED:
             case ETIMEDOUT:
-            case ENETUNREACH:  *err = JFS_ERR_LAN_HOST_UNREACH; break;
-            case EINTR:        *err = JFS_ERR_INTER; break;
-            default:           *err = JFS_ERR_SYS; break;
+            case ENETUNREACH:  *err = JCL_ERR_LAN_HOST_UNREACH; break;
+            case EINTR:        *err = JCL_ERR_INTER; break;
+            default:           *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-size_t jfs_recv(int sock_fd, void *buf, size_t size, int flags, jfs_err_t *err) {
+size_t jfs_recv(int sock_fd, void *buf, size_t size, int flags, jcl_err_t *err) {
     ssize_t status = recv(sock_fd, buf, size, flags);
     if (status == -1) {
         switch (errno) {
-            case EAGAIN: *err = JFS_ERR_AGAIN; break;
-            case EINTR:  *err = JFS_ERR_INTER; break;
-            default:     *err = JFS_ERR_SYS; break;
+            case EAGAIN: *err = JCL_ERR_AGAIN; break;
+            case EINTR:  *err = JCL_ERR_INTER; break;
+            default:     *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(0);
     }
@@ -156,15 +146,15 @@ size_t jfs_recv(int sock_fd, void *buf, size_t size, int flags, jfs_err_t *err) 
     return (size_t) status;
 }
 
-size_t jfs_send(int sock_fd, const void *buf, size_t size, int flags, jfs_err_t *err) {
+size_t jfs_send(int sock_fd, const void *buf, size_t size, int flags, jcl_err_t *err) {
     ssize_t status = send(sock_fd, buf, size, flags);
     if (status == -1) {
         switch (errno) {
-            case EAGAIN:     *err = JFS_ERR_AGAIN; break;
-            case ECONNRESET: *err = JFS_ERR_CONNECTION_RESET; break;
-            case EINTR:      *err = JFS_ERR_INTER; break;
-            case EPIPE:      *err = JFS_ERR_PIPE; break;
-            default:         *err = JFS_ERR_SYS; break;
+            case EAGAIN:     *err = JCL_ERR_AGAIN; break;
+            case ECONNRESET: *err = JCL_ERR_CONNECTION_RESET; break;
+            case EINTR:      *err = JCL_ERR_INTER; break;
+            case EPIPE:      *err = JCL_ERR_PIPE; break;
+            default:         *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(0);
     }
@@ -172,11 +162,11 @@ size_t jfs_send(int sock_fd, const void *buf, size_t size, int flags, jfs_err_t 
     return (size_t) status;
 }
 
-int jfs_socket(int domain, int type, int protocol, jfs_err_t *err) {
+int jfs_socket(int domain, int type, int protocol, jcl_err_t *err) {
     int new_fd = socket(domain, type, protocol);
     if (new_fd == -1) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(-1);
     }
@@ -184,118 +174,118 @@ int jfs_socket(int domain, int type, int protocol, jfs_err_t *err) {
     return new_fd;
 }
 
-void jfs_close(int close_fd, jfs_err_t *err) {
+void jfs_close(int close_fd, jcl_err_t *err) {
     if (close(close_fd) == -1) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr, jfs_err_t *err) {
+void jfs_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr, jcl_err_t *err) {
     if (pthread_mutex_init(mutex, attr) != 0) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_mutex_destroy(pthread_mutex_t *mutex, jfs_err_t *err) {
+void jfs_mutex_destroy(pthread_mutex_t *mutex, jcl_err_t *err) {
     if (pthread_mutex_destroy(mutex) != 0) {
         switch (errno) {
-            case EBUSY: *err = JFS_ERR_MUTEX_BUSY; break;
-            default:    *err = JFS_ERR_SYS; break;
+            case EBUSY: *err = JCL_ERR_MUTEX_BUSY; break;
+            default:    *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_mutex_trylock(pthread_mutex_t *mutex, jfs_err_t *err) {
+void jfs_mutex_trylock(pthread_mutex_t *mutex, jcl_err_t *err) {
     if (pthread_mutex_trylock(mutex) != 0) {
         switch (errno) {
-            case EBUSY: *err = JFS_ERR_MUTEX_BUSY; break;
-            default:    *err = JFS_ERR_SYS; break;
+            case EBUSY: *err = JCL_ERR_MUTEX_BUSY; break;
+            default:    *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_cond_destroy(pthread_cond_t *cond, jfs_err_t *err) {
+void jfs_cond_destroy(pthread_cond_t *cond, jcl_err_t *err) {
     if (pthread_cond_destroy(cond) != 0) {
         switch (errno) {
-            case EBUSY: *err = JFS_ERR_COND_BUSY; break;
-            default:    *err = JFS_ERR_SYS; break;
+            case EBUSY: *err = JCL_ERR_COND_BUSY; break;
+            default:    *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-void jfs_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *time, jfs_err_t *err) {
+void jfs_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *time, jcl_err_t *err) {
     if (pthread_cond_timedwait(cond, mutex, time) != 0) {
         switch (errno) {
-            case ETIMEDOUT: *err = JFS_ERR_COND_TIMED_OUT; break;
-            default:        *err = JFS_ERR_SYS; break;
+            case ETIMEDOUT: *err = JCL_ERR_COND_TIMED_OUT; break;
+            default:        *err = JCL_ERR_SYS; break;
         }
         VOID_RETURN_ERR;
     }
 }
 
-int jfs_eventfd(unsigned int initval, int flags, jfs_err_t *err) {
+int jfs_eventfd(unsigned int initval, int flags, jcl_err_t *err) {
     int event_fd = eventfd(initval, flags);
     if (event_fd == -1) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(-1);
     }
     return event_fd;
 }
 
-size_t jfs_read(int fd, void *buf, size_t size, jfs_err_t *err) {
+size_t jfs_read(int fd, void *buf, size_t size, jcl_err_t *err) {
     ssize_t status = read(fd, buf, size);
     if (status == -1) {
         switch (errno) {
-            case EAGAIN: *err = JFS_ERR_AGAIN; break;
-            case EINTR:  *err = JFS_ERR_INTER; break;
-            default:     *err = JFS_ERR_SYS; break;
+            case EAGAIN: *err = JCL_ERR_AGAIN; break;
+            case EINTR:  *err = JCL_ERR_INTER; break;
+            default:     *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(0);
     }
     return (size_t) status;
 }
 
-size_t jfs_write(int fd, const void *buf, size_t size, jfs_err_t *err) {
+size_t jfs_write(int fd, const void *buf, size_t size, jcl_err_t *err) {
     ssize_t status = write(fd, buf, size);
     if (status == -1) {
         switch (errno) {
-            case EAGAIN: *err = JFS_ERR_AGAIN; break;
-            case EINTR:  *err = JFS_ERR_INTER; break;
-            case EPIPE:  *err = JFS_ERR_PIPE; break;
-            default:     *err = JFS_ERR_SYS; break;
+            case EAGAIN: *err = JCL_ERR_AGAIN; break;
+            case EINTR:  *err = JCL_ERR_INTER; break;
+            case EPIPE:  *err = JCL_ERR_PIPE; break;
+            default:     *err = JCL_ERR_SYS; break;
         }
         VAL_RETURN_ERR(0);
     }
     return (size_t) status;
 }
 
-void *jfs_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off, jfs_err_t *err) {
+void *jfs_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off, jcl_err_t *err) {
     void *mem = mmap(addr, len, prot, flags, fd, off);
     if (mem == MAP_FAILED) {
         switch (errno) {
-            default: *err = JFS_ERR_SYS; break;
+            default: *err = JCL_ERR_SYS; break;
         }
         NULL_RETURN_ERR;
     }
     return mem;
 }
 
-void *jfs_aligned_alloc(size_t align, size_t size, jfs_err_t *err) {
+void *jfs_aligned_alloc(size_t align, size_t size, jcl_err_t *err) {
     void *mem = NULL;
     int   status = posix_memalign(&mem, align, size);
     if (status != 0) {
-        *err = JFS_ERR_SYS;
+        *err = JCL_ERR_SYS;
         NULL_RETURN_ERR;
     }
     return mem;
